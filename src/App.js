b1cd614881bd0794b27c824f75687846a5b0a318
sdelphine <delph.sebast@gmail.com>
2019-12-20 18:00:54 +0100

    Move Router

--- src/App.js
+++ src/App.js
@@ -2,21 +2,25 @@ import React from 'react';
 import './App.css';
 import Home from './components/Home';
 import {
-  BrowserRouter as Router,
   Route,
   Switch,
 } from 'react-router-dom';
 import CreateMessage from './components/CreateMessage';
+import { fakeTopic, fakeMessageList } from './lib/fakeUtils';
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Switch>
-          <Route exact path="/" component={Home} />  
-          <Route path="/newMessage" component={CreateMessage} />
-        </Switch>
-      </Router>
+      <Switch>
+        <Route
+          path={`/${fakeTopic}`}
+          component={() => <Home topic={fakeTopic} messageList={fakeMessageList} />}
+        />
+        <Route
+          path={`/${fakeTopic}/newMessage`}
+          component={() => <CreateMessage topic={fakeTopic} onCreateMessage={(message) => console.log(message)} />}
+        />
+      </Switch>
     </div>
   );
 }
