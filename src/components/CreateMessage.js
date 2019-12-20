233cd94dcf0e1dbf9e6e638b14180b21148f9723
sdelphine <delph.sebast@gmail.com>
2019-12-20 18:02:37 +0100

    CreateMessage behaviour

--- src/components/CreateMessage.js
+++ src/components/CreateMessage.js
@@ -1,52 +1,50 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+import Header from '../components/Header';
 
-function randomId() {
+function randomId() {     
     return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
 }
 
-// TODO when submit, add datetime
-
-class CreateMessage extends Component {
-    state = {
-        message: '',
-        id: '',
-        from: '',
-        topic: '',
-        at: '',
-    }
-
-    handleChange = (e) => {
-        this.setState({ [e.target.name]: e.target.value })
-    }
-
-    routeChange() {
-        let path = `/`;
-        this.props.history.push(path);
-      }
-    
-    render() {
-        return (
-            <div>
-                <label className="form">
-                    <span>From</span>
-                    <select name="from" id="from-select">
-                        <option value="toto">Toto</option>
-                        <option value="titi">Titi</option>
-                    </select>
-                    <textarea
-                        type="text"
-                        id="message"
-                        name="message"
-                        placeholder="Write your message"
-                        required
-                        value={this.state.message}
-                        onChange={this.handleChange}
-                    />
-                    <button type="submit" onClick={this.routeChange.bind(this)}>📮Send</button>
-                </label>
-            </div>
-        );
+function checkMessage(message, sender, func) {
+    if (message && message.startsWith('Bonjour') && sender) {
+        func({ id: randomId(), message: message, sender: sender })
     }
 }
 
-export default CreateMessage;
+// TODO when submit, add datetime
+export default function CreateMessage( { topic, onCreateMessage } ) {
+    const [message, handleMessageChange] = useState("")
+    const [sender, changeSender] = useState("")
+    onCreateMessage = onCreateMessage || (() => {})
+
+    return (
+        <div className="form">
+            <Header topic={topic} />
+            <div className="form-from">
+                <span>From</span>
+                <select name="from" 
+                    id="from-select" 
+                    onChange={(event) => changeSender(event.target.value)}>
+                    <option value="none">None</option>
+                    <option value="user1">User1</option>
+                    <option value="user2">User2</option>
+                </select>
+            </div>
+            <textarea
+                type="text"
+                id="message"
+                name="message"
+                required
+                placeholder="Write your message"
+                value={message}
+                onChange={event => handleMessageChange(event.target.value)}
+            />
+            <button
+                type="button"
+                onClick={() => checkMessage(message, sender, (message) => onCreateMessage(message))}>
+                <span role="img" aria-label="send">📮</span>Send
+            </button>
+        </div>
+    );
+
+}
