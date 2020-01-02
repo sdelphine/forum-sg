8c53e3c35d8be4bc2c1b2b9b4403ec58a8cf1c51
sdelphine <delph.sebast@gmail.com>
2020-01-02 17:19:49 +0100

    Add store in app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,21 +7,25 @@ import {
 } from 'react-router-dom';
 import CreateMessage from './messages/CreateMessage';
 import Home from './misc/Home';
+import { store } from './store/storeModel';
+import { StoreProvider } from 'easy-peasy';
 
 function App() {
   return (
-    <div className="App">
-      <Switch>
-        <Route exact path={"/"} render={() => <Home />}/>
-        <Route exact path={"/:topic"} render={(props) => <Topic { ...props.match.params } />}/>
-        <Route exact path={"/:topic/newMessage"} render={
-          (props) => <CreateMessage
-            { ...props.match.params }
-            onCreateMessage={(message) => console.log(message)} />
-          }
-        />
-      </Switch>
-    </div>
+    <StoreProvider store={store}>
+      <div className="App">
+        <Switch>
+          <Route exact path={"/"} render={() => <Home />}/>
+          <Route exact path={"/:topic"} render={(props) => <Topic { ...props.match.params } />}/>
+          <Route exact path={"/:topic/newMessage"} render={
+            (props) => <CreateMessage
+              { ...props.match.params }
+              onCreateMessage={(message) => console.log(message)} />
+            }
+          />
+        </Switch>
+      </div>
+    </StoreProvider>
   );
 }
 
