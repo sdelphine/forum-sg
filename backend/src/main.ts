aea75f726bcbb212d685a684e34b595874a7d286
sdelphine <delph.sebast@gmail.com>
2020-01-02 17:07:27 +0100

    Manage CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  await app.listen(3001);
 }
 bootstrap();
